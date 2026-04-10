d50ec49f5567ed51d20dd02a3b9f9a7a6e479614
Nico Ricaldi <nicoricaldi.dev@gmail.com>
2026-04-09 21:07:37 -0400

    Initial Commit

---
+++ taskTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  taskTracker
+//
+//  Created by Nico Ricaldi on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ taskTracker/taskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskTrackerApp.swift
+//  taskTracker
+//
+//  Created by Nico Ricaldi on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct taskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
